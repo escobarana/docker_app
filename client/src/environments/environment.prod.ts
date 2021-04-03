794665e8b20183f721d178fd2c3fa35f0544bc11
Ana Escobar <anaescobar8@gmail.com>
2021-04-03 02:53:18 +0200

    Update: client files

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment_server = {
   production: true,
   //apiUrl: 'https://recommended-server.herokuapp.com'
-  apiUrl: 'http://156.35.163.172:3000'
+  apiUrl: 'http://localhost:3000'
 };
 
 export const environment_r = {
   production: true,
-  apiUrl: 'https://beta.rstudioconnect.com/content:9519'
+  apiUrl: 'http://localhost:7190/'
 };
